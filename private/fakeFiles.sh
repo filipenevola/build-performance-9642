4fd795c6df5ff1198ec9681b8d02d7cb3e2b12bf
filipe_nevola <filipe.nevola@tecsinapse.com.br>
2018-02-15 17:03:22 -0300

    creating fake files (commit only with 3 fake files but we have tested with 1000, 2000 and 5000 (see txt files on private)

---
+++ private/fakeFiles.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# to run
+# yarn run start-speed > ../../../running-app-logs-5_000-fake-files.txt
+
+# to tail
+# tail -f -n 5000 Documents/pathable/running-app-logs-5_000-fake-files.txt
+
+# to generate fake files and import then (just import index from fake folder somewhere)
+echo "" > "../imports/fake/index.js"
+for i in `seq 1 3`;
+        do
+                echo "export const x$i = () => {}" > "../imports/fake/$i.js"
+                echo "import './$i';" >> "../imports/fake/index.js"
+        done
