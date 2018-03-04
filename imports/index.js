3147467728824adb5cd4a26314087011b75e8b14
Nacho Codoñer <igcogi@gmail.com>
2018-03-04 10:17:47 +0100

    prepare script to generate env for the experiment

---
+++ imports/index.js
@@ -0,0 +1,4 @@
+
+import { x1 } from './fake';
+import { x1 as xA } from 'meteor/a';
+import { x1 as xB } from 'meteor/b';
---
+++ packages/b/package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  version: '1.0.0',
+  name: 'b',
+});
+
+Package.onUse((api) => {
+  api.versionsFrom('1.6.1');
+
+  api.use('ecmascript');
+
+  api.addFiles('index.js', ['client', 'server']);
+});
